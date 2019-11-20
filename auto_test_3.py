1d4759e6e04dcfd92ef0e796e8b11bf017b058eb
AleksanderDobek <alek.dobek@gmail.com>
2019-11-20 15:07:15 +0100

    KP new tests

--- auto_test_3.py
+++ auto_test_3.py
@@ -92,7 +92,53 @@ class LoginPageTests(unittest.TestCase):
         warning = driver.find_element_by_xpath('//*[@class="error"]')
         warning_text = warning.text
         self.assertEqual("identyfikator ma min. 8 znaków", warning_text,
-                         f'Expected button state differs from actual for page url: {url}')
+                         f'Expected warning message differs from actual for page url: {url}')
+
+    def test_demo_main_page_check_respond_when_input_is_ok(self):
+        driver = self.driver
+        url = 'https://demobank.jaktestowac.pl/logowanie_etap_1.html'
+        driver.get(url)
+        ident_text_field = driver.find_element_by_id('login_id')
+        ident_text_field.send_keys(12345678)
+        login_next = driver.find_element_by_xpath('//*[@id="login_next"]')
+        login_next.click()
+        time.sleep(3)
+        login_next_button_element = driver.find_element_by_xpath('//*[@id="login_next"]')
+        new_login_button_text = login_next_button_element.text
+        self.assertEqual('zaloguj się', new_login_button_text,
+                         f'Expected login button text: zaloguj się , differ from actual {new_login_button_text}')
+
+    def test_demo_main_page_check_message_after_remind_identifier_click(self):
+        driver = self.driver
+        url = 'https://demobank.jaktestowac.pl/logowanie_etap_1.html'
+        driver.get(url)
+        identifier_remind_button = driver.find_element_by_id('ident_rem')
+        identifier_remind_button.click()
+        time.sleep(3)
+        identifier_remind_message_element = driver.find_element_by_xpath('//*[@class="shadowbox-content contact-popup"]')
+        identifier_remind_message_element_text = identifier_remind_message_element.text
+        identifier_remind_message_element_close_button = driver.find_element_by_xpath('//i[@class="shadowbox-close"]')
+        identifier_remind_message_element_close_button.click()
+        self.assertEqual("ta funkcja jest niedostępna", identifier_remind_message_element_text,
+                         f'Expected message differs from actual for page url: {url}')
+
+    def test_demo_main_page_check_if_loged_in_after_correct_login_and_password_input(self):
+        driver = self.driver
+        url = 'https://demobank.jaktestowac.pl/logowanie_etap_2.html'
+        driver.get(url)
+        identifier_text_field = driver.find_element_by_id('login_id')
+        identifier_text_field.send_keys(12345678)
+        password_text_field = driver.find_element_by_id('login_password')
+        password_text_field.send_keys(87654321)
+        login_next_button_element = driver.find_element_by_xpath('//*[@id="login_next"]')
+        login_next_button_element.click()
+        time.sleep(3)
+        new_page_element = driver.find_element_by_xpath('//div[@class="category active"]//*[@class="show-page-loader"]')
+        new_page_element_text = new_page_element.text
+        self.assertEqual("mój pulpit", new_page_element_text,
+                         f'Expected new element text differs from actual for page url: {url}')
+
+
     @classmethod
     def tearDownClass(self):
         self.driver.quit()
